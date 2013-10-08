0a05efd7561befa6679f20c97f98052b3d2d2553
Daniel V. Tan <daniel.tan@nuworks.ph>
2013-10-08 13:12:40 +0800

    Updated code. Added Comments

--- js/EventDispatcher.js
+++ js/EventDispatcher.js
@@ -1,11 +1,48 @@
+/** Class: EventDispatcher
+    an extendable event dispatcher class
+	
+    (start code)
+    // to extend add this in inside your class
+	YourClass.prototype = EventDispatcher;
+	YourClass.prototype.constructor = YourClass;
+	EventDispatcher.apply(this, arguments);
+    
+    // returns an instance of EventDispatcher class
+	var eventD = new YourClass();
+	// adds an event
+	eventD.addEventListener("ON_USER_SCROLL", callBackFunction);
+	// dispatches the event to the listeners triggers the callBackFunction
+	dispatchEvent("ON_USER_SCROLL", addDataToPass);
+	
+    (end)
+*/
 function EventDispatcher() {
+    /** this reference */
 	var thisClass = this;
+    /** check if the browser is IE
+		@type {Boolean}
+	*/
 	this.isIE = (navigator.appName == 'Microsoft Internet Explorer') ? true: false;
-	this.defaultTarget = window;
+    /** set defaultTarget
+		@type {dom}	
+	*/
+	this.defaultTarget;
+    /** list of event listeners
+		@type {Object}
+		@protected
+	*/
 	this.listeners = {};
+    /** list of liveListeners
+		@type {Array}
+		@protected
+	*/
 	this.liveListener = [];
+    /** use to get cross browser mouse x and y position
+		@param {Object} e - event object from mouse events
+		@returns Object x and y
+	*/
 	this.mouse = function(e){
-		var mouse = new Object();
+		var mouse = {};
 		
 		if(e){
 			if(e.custom){
@@ -35,6 +72,10 @@ function EventDispatcher() {
 		}
 		return mouse;
 	}
+    /** add a live listener
+		@param {String} id - any unique string
+		@param {Function} func - callback function
+	*/
 	this.addLiveListener = function(id, func) {
         var eventInterval;
         var live = {id:id, func:func, eventInterval:eventInterval };
@@ -52,6 +93,9 @@ function EventDispatcher() {
 			}
 		}
 	}
+    /** remove a live listener
+		@param {Object} live - object that was added
+	*/
 	this.removeLiveListener = function(live) {
         var eventInterval = live.eventInterval;
 		var index = thisClass.liveListener.indexOf(live);
@@ -59,9 +103,18 @@ function EventDispatcher() {
 		if(thisClass.liveListener.length <= 0)
 			clearInterval(eventInterval);
 	}
+    /** use to get crossbrowser keyCode
+		@param {Object} e - event object from keyboard events
+		@returns keyCode
+	*/
 	this.keyCode = function(e) {
 		return (e != undefined) ? e.keyCode : window.event.keyCode;
 	}
+    /** use to dispatch events
+		@param {String} type - string that was used when adding the event listener
+		@param {Object} e - data to pass
+		@protected
+	*/
 	this.dispatchEvent = function(type, e){
 		if(thisClass.listeners[type] == undefined)
 			return;
@@ -69,31 +122,34 @@ function EventDispatcher() {
 			thisClass.listeners[type][i](e);
 		}
 	}
+    /** use to add event listener
+		@param {String} type - any unique string
+		@param {Function} listener - callback function
+		@param {dom} target - dom element
+	*/
 	this.addListener = function(type, listener, target){
 		if(thisClass.listeners[type] == undefined) 
 			thisClass.listeners[type] = [];
 		thisClass.listeners[type].push(listener);
-		
-		var t = (target != null) ? target : thisClass.defaultTarget;
-		if(t[type] == undefined)
-			return;
+        
+		var t = target ? target : thisClass.defaultTarget;
+        
+		if(!t || t[type] === undefined)
+            return;
+        if(t == window)
+            console.log("Caution: adding " + type + " event on ->" + window);
 		t[type] = function(e){
-			if(t != thisClass.defaultTarget)
-				t.dispatchEvent(type, e);
-			else
-				thisClass.dispatchEvent(type, e);
+            thisClass.dispatchEvent(type, e);
 		};
 	}
+    /** use to remove event listener
+		@param {String} type - string that was used when adding the event listener
+		@param {Function} listener - callback function
+		@param {dom} - null
+	*/
 	this.removeListener = function(type, listener, target){
 		var index = thisClass.listeners[type].indexOf(listener);
 		thisClass.listeners[type].splice(index, 1);
     }
 	
 }
-/** Event: NuEvent
-	LOAD - triggers when browser is loaded completely
-*/
-var NuEvent = {
-			LOAD:"onload"
-
-			};
